b0a0d29af9753c769153a900bf0c888591cd51fa
rabibasukala01 <ibarbasukala77@gmail.com>
2025-04-17 12:55:17 +0545

    yt/tiktok publisher

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from video_creation.background import (
 from video_creation.final_video import make_final_video
 from video_creation.screenshot_downloader import get_screenshots_of_reddit_posts
 from video_creation.voices import save_text_to_mp3
-from uploads import tiktok,youtube
+from uploads import tiktok, youtube
 
 __VERSION__ = "3.3.0"
 
@@ -60,23 +60,25 @@ def main(POST_ID=None) -> None:
     make_final_video(number_of_comments, length, reddit_object, bg_config)
 
     # uploads
-    viddata='./video_creation/data/videos.json'
-    with open(viddata, 'r') as f:
+    viddata = "./video_creation/data/videos.json"
+    with open(viddata, "r") as f:
         data = json.load(f)
 
     # Now you can use the data
-    title=data[-1]['reddit_title']
-    filename=data[-1]['filename']
-    subreddit=data[-1]['subreddit']
+    title = data[-1]["reddit_title"]
+    filename = data[-1]["filename"]
+    subreddit = data[-1]["subreddit"]
     VIDEO_FILE_PATH = f"./results/{subreddit}/{filename}"
-    
+
     # youtube upload
-    print_step("Uploading to youtube...")
-    TITLE = title
+    print_step(f"Uploading to youtube...with title {title}")
+    TITLE = title[:95] + "..."  # 100 character limit
     DESCRIPTION = title
-    CATEGORY_ID = '24'  # 24 = Entertainment
-    TAGS = ['reddit', 'jokes', 'epic', 'viral']
-    vid_id=youtube.upload_video(VIDEO_FILE_PATH,TITLE,DESCRIPTION,CATEGORY_ID,TAGS)
+    CATEGORY_ID = "24"  # 24 = Entertainment
+    TAGS = ["reddit", "jokes", "epic", "viral"]
+    vid_id = youtube.upload_video(
+        VIDEO_FILE_PATH, TITLE, DESCRIPTION, CATEGORY_ID, TAGS
+    )
 
     # tiktok upload
     print_step("Uploading to tiktok...")
@@ -99,6 +101,7 @@ def shutdown() -> NoReturn:
 
     print("Exiting...")
     sys.exit()
+
 
 def gen():
     ffmpeg_install()
--- uploads/youtube.py
+++ uploads/youtube.py
@@ -1,4 +1,3 @@
-
 """
 check the docs for the youtube api
 https://developers.google.com/youtube/v3/getting-started
@@ -16,52 +15,51 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.http import MediaFileUpload
 
 # The scopes required by the API
-SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
 
 # The client_secrets.json file is obtained from the Google Cloud Console
-CLIENT_SECRET_FILE = r'.\uploads\yt-secrets.json'
+CLIENT_SECRET_FILE = r".\uploads\yt-secrets.json"
 
 # YouTube API service name and version
-API_SERVICE_NAME = 'youtube'
-API_VERSION = 'v3'
+API_SERVICE_NAME = "youtube"
+API_VERSION = "v3"
 
 
 def authenticate_youtube():
     # Load client secrets file, put the path of your file
-    print('Authenticating YouTube...')
-    flow = InstalledAppFlow.from_client_secrets_file(
-        CLIENT_SECRET_FILE, SCOPES)
+    print("Authenticating YouTube...")
+    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
     credentials = flow.run_local_server()
 
-    youtube = build(
-        "youtube", "v3", credentials=credentials)
+    youtube = build("youtube", "v3", credentials=credentials)
 
     return youtube
 
-def upload_video(VIDEO_FILE_PATH,TITLE,DESCRIPTION,CATEGORY_ID,TAGS):
+
+def upload_video(VIDEO_FILE_PATH, TITLE, DESCRIPTION, CATEGORY_ID, TAGS):
     youtube = authenticate_youtube()
     """Upload a video to YouTube."""
     # Prepare the file for upload
-    media = MediaFileUpload(VIDEO_FILE_PATH, mimetype='video/mp4', chunksize=-1, resumable=True)
+    media = MediaFileUpload(
+        VIDEO_FILE_PATH, mimetype="video/mp4", chunksize=-1, resumable=True
+    )
 
     # Request body for the upload
     request_body = {
-        'snippet': {
-            'title': TITLE,
-            'description': DESCRIPTION,
-            'tags': TAGS,
-            'categoryId': CATEGORY_ID,
+        "snippet": {
+            "title": TITLE,
+            "description": DESCRIPTION,
+            "tags": TAGS,
+            "categoryId": CATEGORY_ID,
         },
-        'status': {
-            'privacyStatus': 'private',  # TODOYou can change it to 'public' or 'unlisted'
-        }
+        "status": {
+            "privacyStatus": "private",  # TODOYou can change it to 'public' or 'unlisted'
+        },
     }
 
     # Upload the video
     request = youtube.videos().insert(
-        part="snippet,status",
-        body=request_body,
-        media_body=media
+        part="snippet,status", body=request_body, media_body=media
     )
 
     # Monitor the upload process
@@ -69,11 +67,11 @@ def upload_video(VIDEO_FILE_PATH,TITLE,DESCRIPTION,CATEGORY_ID,TAGS):
     print(f'Upload successful to youtube! Video ID: {response["id"]}')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # The video you want to upload
-    VIDEO_FILE_PATH = 'test.mp4'
-    TITLE = 'this is a Title'
-    DESCRIPTION = 'Video Description'
-    CATEGORY_ID = '24'  # 24 = Entertainment
-    TAGS = ['tags', 'tags', 'tags', 'tags']
-    vid_id=upload_video(VIDEO_FILE_PATH,TITLE,DESCRIPTION,CATEGORY_ID,TAGS)
+    VIDEO_FILE_PATH = r"D:\Python\reditpost\Reddit-video-bot\results\jokes+dadjokes\When my great-grandfather went bald he built a machine to weave a wig out of yarn He gave it to my grandfather who then gave it to my dad and one day it will be mine.mp4"
+    TITLE = "When my greatgrandfather went bald he built a machine to..."
+    DESCRIPTION = "When my great-grandfather went bald he built a machine to weave a wig out of yarn He gave it to my grandfather who then gave it to my dad and one day it will be mine"
+    CATEGORY_ID = "24"  # 24 = Entertainment
+    TAGS = ["redit", "funny", "comments", "reditpost"]
+    vid_id = upload_video(VIDEO_FILE_PATH, TITLE, DESCRIPTION, CATEGORY_ID, TAGS)
